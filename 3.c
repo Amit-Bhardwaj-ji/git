a2aff72d04840567a75a33b62d32a3845453af13
Amit Bhardwaj <21cs1001@mvn.edu.in>
2022-04-11 12:34:43 +0530

    Pandut ji

---
+++ 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter any two no. :");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of two no. is :%d",c);
+    
+
+}
+
+
+ 
